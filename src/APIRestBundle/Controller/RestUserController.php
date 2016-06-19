d74a93478df1bda5ffbfc572ff60d97545afb091
LeoHugues <leo.hugues@hotmail.fr>
2016-06-19 07:21:34 -0700

    mise en place du login sur l'api

--- src/APIRestBundle/Controller/RestUserController.php
+++ src/APIRestBundle/Controller/RestUserController.php
@@ -13,6 +13,11 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation as Doc;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use UserBundle\Entity\User;
 use JMS\Serializer\Annotation\ExclusionPolicy;
 use JMS\Serializer\Annotation\Expose;
@@ -62,5 +67,63 @@ class RestUserController extends FOSRestController
         $user = $em->getRepository('UserBundle:User')->find($userId);
 
         return array('user' => $user);
+    }
+
+    /**
+     *
+     * @Rest\Post("/user/login", name="agenda_api_login")
+     * @Doc\ApiDoc(
+     *     section="User",
+     *     resource=true,
+     *     description="Get user by id.",
+     *     statusCodes={
+     *          200="Returned when successful",
+     *     },
+     *     requirements={
+     *      {"name" = "username", "dataType" = "string", "requirement"="\d+", "description"="nom d'utilisateur"},
+     *      {"name" = "password", "dataType" = "string", "requirement"="\d+", "description"="mot de passe de l'utilisateur"}
+     *  },
+     * )
+     */
+    public function loginAction(Request $request)
+    {
+        $username = $request->get("username");
+        $password = $request->get("password");
+
+        if(is_null($username) || is_null($password)) {
+            return new JsonResponse(
+                ["error" => 'Please verify all your inputs.'],
+                Response::HTTP_UNAUTHORIZED,
+                array('Content-type' => 'application/json')
+            );
+        }
+
+        $user_manager = $this->get('fos_user.user_manager');
+        $factory = $this->get('security.encoder_factory');
+
+        $user = $user_manager->findUserByUsername($username);
+        if (is_null($user)) {
+            return new JsonResponse(
+                ["error" => 'Username or Password not valid.'],
+                Response::HTTP_UNAUTHORIZED,
+                array('Content-type' => 'application/json')
+            );
+        }
+        $encoder = $factory->getEncoder($user);
+        $salt = $user->getSalt();
+
+        if($encoder->isPasswordValid($user->getPassword(), $password, $salt)) {
+            return new JsonResponse(
+                ["status" => 'CONNECTED'],
+                Response::HTTP_OK,
+                array('Content-type' => 'application/json')
+            );
+        } else {
+            return new JsonResponse(
+                ["error" => 'Username or Password not valid.'],
+                Response::HTTP_UNAUTHORIZED,
+                array('Content-type' => 'application/json')
+            );
+        }
     }
 }
